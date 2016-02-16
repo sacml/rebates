6d3e09712ee11be9ab7b18a5eb2fe0d9e2a06350
joshh <josh.hendrick@ca.com>
2016-02-16 00:20:52 -0500

    Fixed menu staying open

--- packages/custom/greenohm/public/assets/js/custom.js
+++ packages/custom/greenohm/public/assets/js/custom.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
 	var h_header = $('#header').outerHeight();
 	$('#content').css('margin-top',h_header);
 	$('.popup-compare').css('margin-top',-h_header);
+	
 		$('.toogle-menu').click(function(event){
 		$(this).parents('.main-menu').find('ul').slideToggle();
 		event.preventDefault();
 	});
+	
+	$("a").click(function(event) {
+    $(this).parents('.main-menu').find('ul').slideUp();
+  });
+  
 	
 });
 $(document).load(function(){
